cee917668f4260da8a1d3acc41e942acbd998256
RykerD7667 <ryker.dickson@dcmail.ca>
2025-07-31 20:03:19 -0400

    added image arrays and looping function to assignment 4 part 2 java

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -12,14 +12,26 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const imageFilenames = [
+    'pic1.jpg',
+    'pic2.jpg',
+    'pic3.jpg',
+    'pic4.jpg',
+    'pic5.jpg'
+];
 /* Declaring the alternative text for each image file */
-
+const imageAlts = [
+    'Closeup of a human eye',
+    'Rock that looks like a wave',
+    'Purple and white pansies',
+    'Section of wall from a pharaoh\'s tomb',
+    'Large moth on a leaf'
+];
 /* Looping through images */
+for (let i = 0; i < imageFilenames.length; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/' + imageFilenames[i]);
+    newImage.setAttribute('alt', imageAlts[i]);
+    thumbBar.appendChild(newImage);
+}
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
-/* Wiring up the Darken/Lighten button */
